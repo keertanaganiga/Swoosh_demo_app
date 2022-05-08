a56a92f9b2b19d07e732bafb108fbf8233aa78e5
keertanaganiga <keertanaganiga@gmail.com>
2022-05-08 13:50:32 +0530

    Lifecycle test

--- app/src/main/java/com/keertana/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/keertana/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.keertana.swoosh
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
