eb42180bbc7f90823dc85f1f8278f82e7c6d0f5e
keertanaganiga <keertanaganiga@gmail.com>
2022-05-08 14:31:43 +0530

    third page

---
+++ app/src/main/java/com/keertana/swoosh/SkillActivity.kt
@@ -0,0 +1,11 @@
+package com.keertana.swoosh
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
