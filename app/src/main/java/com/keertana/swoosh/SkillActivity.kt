4640b88048f7b16c149518d09b3902eb7f95b743
keertanaganiga <keertanaganiga@gmail.com>
2022-05-08 20:56:19 +0530

    created constants file and added extra league intent

--- app/src/main/java/com/keertana/swoosh/SkillActivity.kt
+++ app/src/main/java/com/keertana/swoosh/SkillActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SkillActivity : AppCompatActivity() {
+
+    var league=""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league= intent.getStringExtra(EXTRA_LEAGUE).toString()
+        println(league)
     }
 }
